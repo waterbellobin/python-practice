f00945caaa68cb619dcda3dbe8be0a955227c0de
waterbellobin <lgjong951@naver.com>
2020-05-15 22:11:47 +0900

    data structure stuff

---
+++ practice.py
@@ -0,0 +1,74 @@
+# print(5)
+# print(3.14)
+# print("ha"*9)
+# print(5<10)
+
+# age = 5
+# name = "yo"
+
+# print(name + " is " + str(age) + "years old")
+# print(name, "is", age, "years old")
+
+# print(2**3)
+# print(29//4)
+# print(29%4)
+
+# print((3 < 4) and (5 < 2))
+# print((3 < 4) & (5 < 2))
+
+# print((3 < 4) or (5 < 2))
+# print((3 < 4) | (5 < 2))
+
+# print(pow(3, 2))
+
+# from math import *
+
+# print(floor(4.89))
+# print(ceil(3.19))
+# print(sqrt(16))
+
+# from random import *
+
+# print(random()) # 0.0 ~ 1.0 미만
+# print(int(random() * 10)) # 0 ~ 10 미만
+# print(int(random() * 10 + 1)) # 1 ~ 10 이하
+# print(randrange(1, 46)) # 1 ~ 46 미만
+# print(randint(1, 45)) # 1 ~ 45 이하
+
+# sentence = '''
+# I am a boy.
+# I like python.'''
+# print(sentence)
+
+# jumin_number = "940912-1234567"
+# print("gender: " + jumin_number[7])
+# print("year: " + jumin_number[0:2])
+# print("birth year, month, day: " + jumin_number[:6])
+# print("back seven numbers: " + jumin_number[-7:])
+
+# python = "Python is good"
+# print(python.lower())
+# print(python[0].isupper())
+# print(python.replace("Python", "Java"))
+
+# index = python.index("o")
+# print(index)
+# index = python.index("o", index + 1)
+# print(index)
+# print(python.count("o"))
+
+# print("I am %s. I am %dyears old." %("Sujong", 27))
+# print("I am {}years old." .format(27))
+# print("I am {1}. I am {0}years old." .format(27, "Sujong"))
+# print("I am {name}. I am {age}years old." .format(age = 27, name = "Sujong"))
+
+# print("I am Sujong.\nI am 27 years old.")
+# print("I like \"Python\".")
+
+# print("C:\\Users\\Sujong\\Desktop") # \\ is \ in printing
+
+# print("Red apple\rPine") # \r moves cursor to the beginning
+# print("Pinee\bapple") # \b is backspace
+# print("Pine\tapple") # \t is tab
+
+1:22:17
